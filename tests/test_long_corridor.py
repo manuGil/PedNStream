aeddf3f8888fb3d40a8e45dc074419df467413ff
Manuel Garcia <gilbert.mg22@gmail.com>
2026-02-12 14:03:13 +0100

    Refactor test_long_corridor.py: clean up imports and remove commented code

--- tests/test_long_corridor.py
+++ tests/test_long_corridor.py
@@ -5,19 +5,21 @@ Test basic functionality of PedNStream
 # Add project root to Python path
 import os
 import sys
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_root)
-
+import matplotlib
+matplotlib.use('macosx')
 import numpy as np
 import matplotlib.pyplot as plt
 from handlers.output_handler import OutputHandler
 
 # Now you can import using the project structure
-from src.utils.visualizer import NetworkVisualizer, progress_callback
+from src.utils.visualizer import NetworkVisualizer
 from src.LTM.network import Network
 from pathlib import Path
 
 from pytest import fixture
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
 
 @fixture
 def adj():    
@@ -66,95 +68,14 @@ def params():
 def network_env(params,adj):
     
     return Network(adj, params, origin_nodes=[5, 0])
-    # # # Set demand for nodes to zero after a certain time step
-    # network_env.nodes[0].demand[300:] = np.zeros(600 - 300)
-    # network_env.nodes[5].demand[300:] = np.zeros(600 - 300)
-    # ''' Scenario 2 '''
-    # params = {
-    #     'unit_time': 10,
-    #     'simulation_steps': 1200,
-    #     'assign_flows_type': 'classic',
-    #     'default_link': {
-    #         'length': 50,  # make it to 50 to see spillback
-    #         'width': 2,
-    #         'free_flow_speed': 1.1,
-    #         'k_critical': 1,
-    #         'k_jam': 6,
-    #         'activity_probability': 0,  # probability of activity on the link
-    #         'fd_type': 'yperman',  # type of fundamental diagram
-    #         'bi_factor': 1,  # factor for bi-directional FD
-    #         'speed_noise_std': 0,  # whether to add noise to the speed
-    #         'controller_type': 'gate',  # type of controller
-    #     },
-    #     'links': {
-    #         '2_3': {
-    #             'length': 50,
-    #             'width': 1,
-    #             'free_flow_speed': 1.1,
-    #             'k_critical': 2,
-    #             'k_jam': 6,
-    #             'activity_probability': 0,  # probability of activity on the link
-    #             'fd_type': 'yperman',  # type of fundamental diagram
-    #             'bi_factor': 1,  # factor for bi-directional FD
-    #             'speed_noise_std': 0,  # whether to add noise to the speed
-    #             'controller_type': 'gate',  # type of controller
-    #         },
-    #     },
-    #     'demand': {
-    #         "origin_3": {
-    #             "peak_lambda": 20,
-    #             "base_lambda": 8,
-    #         },
-    #         "origin_2": {
-    #             "peak_lambda": 20,
-    #             "base_lambda": 8,
-    #         }
-    #     }
-    #
-    # }
-    # network_env = Network(adj, params, origin_nodes=[2, 3])
-    # network_env.update_turning_fractions_per_node(node_ids=[2, 3],
-    #                                                       new_turning_fractions=np.array([[0, 1, 0.5, 0.5, 0, 1],
-    #                                                                                       [1, 0, 0, 1, 0.5, 0.5]]))
-    # Set demand for nodes to zero after a certain time step
-    # network_env.nodes[2].demand[0:10] = np.zeros(10)
-    # network_env.nodes[3].demand[40:] = np.zeros(1200 - 40)
-    # network_env.nodes[2].demand[80:100] = np.zeros(20)
-    # network_env.nodes[3].demand[30:] = np.zeros(600 - 30)
 
     ## network_env.visualize() # TODO: this is not necessary in testing
 
 class TestLongCorridor:
-    # Run simulation
-    # network_env.links[(3,4)].back_gate_width = 0
-    # network_env.links[(2,1)].back_gate_width = 0
-
-    def test_simulation(self, params, network_env):
+    def test_run_simulation(self, params, network_env):
+        """Test simulation case for long corridor"""
         for t in range(1, params['simulation_steps']):
             network_env.network_loading(t)
-        # if t == 120:
-            # network_env.links[(3,4)].back_gate_width = 1
-            # network_env.links[(2,1)].back_gate_width = 0
-        # if t == 32:
-        #     network_env.links[(3,4)].back_gate_width = 0
-        #     network_env.links[(2,1)].back_gate_width = 0
-        #     network_env.nodes[0].demand[201:251] = np.random.poisson(lam=np.ones(50) * 10, size=50)
-
-    # # Plot inflow and outflow
-    # plt.figure(1)
-    # path = [(2, 3)]
-    # for link_id in path:
-    #     plt.plot(network_env.links[link_id].inflow, label=f'inflow{link_id}')
-    #     plt.plot(network_env.links[link_id].outflow, label=f'outflow{link_id}')
-    #     plt.legend()
-    # plt.show()
-
-    # plt.figure(2)
-    # for link_id in path:
-    #     plt.plot(network_env.links[link_id].cumulative_inflow, label=f'cumulative_inflow{link_id}')
-    #     plt.plot(network_env.links[link_id].cumulative_outflow, label=f'cumulative_outflow{link_id}')
-    #     plt.legend()
-    # plt.show()
 
         # Construct paths relative to the project root
         project_root = Path(__file__).resolve().parent.parent
@@ -165,36 +86,10 @@ class TestLongCorridor:
         output_handler = OutputHandler(base_dir=str(output_dir), simulation_dir=f"{sim_name}")
         output_handler.save_network_state(network_env)
 
-    
-
-        import matplotlib
-        import matplotlib.pyplot as plt
-        from matplotlib.animation import PillowWriter
-        from tqdm import tqdm
-        matplotlib.use('macosx')
-
         # Create the visualization
         visualizer = NetworkVisualizer(simulation_dir=os.path.join(output_dir, f"{sim_name}"))
         anim = visualizer.animate_network(start_time=0, end_time=params["simulation_steps"],
                                     interval=100, edge_property='density', tag=False, vis_actions=True)
 
-    # GIf
-    # writer = PillowWriter(fps=8, metadata=dict(artist='Me'))
-
-    # Save the animation with progress tracking
-    # anim.save(os.path.join(output_dir, f"{sim_name}", "network_animation.gif"),
-    #           writer=writer,
-    #           progress_callback=progress_callback)
-
-    # plt.show()
-    
-    # MP4
-    # writer = matplotlib.animation.FFMpegWriter(fps=15, metadata=dict(artist='Me'),
-    #                                          bitrate=2000)
-    #
-    # # Save the animation as MP4
-    # anim.save(os.path.join(output_dir, "long_corridor", "network_animation.mp4"),
-    #           writer=writer,
-    #           progress_callback=progress_callback)
 
         plt.show()
